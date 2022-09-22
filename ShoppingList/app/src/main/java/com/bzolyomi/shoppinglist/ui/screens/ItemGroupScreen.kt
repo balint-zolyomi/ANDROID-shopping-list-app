e7cd1d40964f3f519bd5847eabbeb075e18809e7
balint-zolyomi <bzolyomi@gmail.com>
2022-09-22 11:30:58 +0200

    add animation to group card

--- ShoppingList/app/src/main/java/com/bzolyomi/shoppinglist/ui/screens/ItemGroupScreen.kt
+++ ShoppingList/app/src/main/java/com/bzolyomi/shoppinglist/ui/screens/ItemGroupScreen.kt
@@ -1,16 +1,20 @@
 package com.bzolyomi.shoppinglist.ui.screens
 
-import android.util.Log
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.animation.*
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.bzolyomi.shoppinglist.data.ShoppingListEntity
 import com.bzolyomi.shoppinglist.viewmodels.SharedViewModel
@@ -21,10 +25,8 @@ fun ItemGroupScreen(
 ) {
     val shoppingGroupsWithLists by sharedViewModel.shoppingGroupsWithLists.collectAsState()
 
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        for (shoppingGroupWithList in shoppingGroupsWithLists) {
-            Log.d("debug-balint", shoppingGroupWithList.toString())
-
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = shoppingGroupsWithLists) { shoppingGroupWithList ->
             GroupCard(
                 title = shoppingGroupWithList.group.groupName,
                 shoppingList = shoppingGroupWithList.shoppingList
@@ -36,7 +38,10 @@ fun ItemGroupScreen(
 @Composable
 fun GroupCard(title: String, shoppingList: List<ShoppingListEntity>) {
 
-    var expanded by remember { mutableStateOf(false) }
+    var expanded by rememberSaveable { mutableStateOf(false) }
+    val categoryBottomDp by animateDpAsState(
+        if (expanded) 0.dp else 24.dp
+    )
 
     Surface(
         color = MaterialTheme.colors.primary,
@@ -45,28 +50,54 @@ fun GroupCard(title: String, shoppingList: List<ShoppingListEntity>) {
             vertical = 4.dp, horizontal = 8.dp
         )
     ) {
-        Row(
-            modifier = Modifier
-                .padding(24.dp)
-        ) {
-            Column(
+        Column {
+            Row(
                 modifier = Modifier
-                    .weight(1f)
+                    .padding(start = 24.dp, top = 24.dp, end = 24.dp, bottom = categoryBottomDp)
             ) {
-                Text(
-                    text = title,
-                    style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.ExtraBold)
-                )
-                if (expanded) {
-                    for (item in shoppingList) {
-                        Text(text = item.itemName)
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                ) {
+                    Text(
+                        text = title,
+                        style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.ExtraBold)
+                    )
+                }
+                Column {
+                    OutlinedButton(
+                        onClick = { expanded = !expanded }
+                    ) {
+                        Text(text = if (expanded) "Show less" else "Show more")
                     }
                 }
             }
-            OutlinedButton(
-                onClick = { expanded = !expanded }
+            AnimatedVisibility(
+                visible = expanded,
+                enter =  expandVertically(
+                    // Expand from the top.
+                    expandFrom = Alignment.Top
+                ) + fadeIn(
+                    // Fade in with the initial alpha of 0.3f.
+                    initialAlpha = 0.0f,
+                    animationSpec = tween(durationMillis = 200)
+                ),
+                exit = shrinkVertically() + fadeOut(animationSpec = tween(durationMillis = 100))
             ) {
-                Text(text = if (expanded) "Show less" else "Show more")
+                Row(
+                    modifier = Modifier.padding(
+                        start = 24.dp,
+                        top = 0.dp,
+                        end = 24.dp,
+                        bottom = 24.dp
+                    )
+                ) {
+                    Column {
+                        for (item in shoppingList) {
+                            Text(text = item.itemName)
+                        }
+                    }
+                }
             }
         }
     }
